6d52818c6cb1cf49b837a69b745f03278161e6f9
lemoer <git@irrelefant.net>
2021-01-05 08:34:01 +0100

    gluon-controller: reference by nodeid

--- package/gluon-controller/luasrc/lib/gluon/controller/controller/controller.lua
+++ package/gluon-controller/luasrc/lib/gluon/controller/controller/controller.lua
@@ -20,22 +20,22 @@ end
 local remotes = {}
 local uci = require('simple-uci').cursor()
 uci:foreach('gluon-controller', 'remote', function(remote)
-	remotes[tonumber(remote.index)] = remote
+	table.insert(remotes, remote)
 end)
 
 -- redirect from controller/ to something
 -- TODO: what to do if no node is configured!
 -- TODO: what to do if no node with index 1 is configured
-entry({}, redirect({"node", "1"}))
+entry({}, redirect({"node", remotes[1].nodeid}))
 
 -- register routes for the remotes
-for i, remote in pairs(remotes) do
+for index, remote in pairs(remotes) do
 	local hostname = 'localhost'
 
-	entry({"node", tostring(remote.index)},
-		redirect({"node", tostring(remote.index), "status"}), _("Information"), 1)
-	entry({"node", tostring(remote.index), "status"},
+	entry({"node", remote.nodeid},
+		redirect({"node", remote.nodeid, "status"}), _("Information"), 1)
+	entry({"node", remote.nodeid, "status"},
 		iframe('http://['..remote.address..']/'), _("Information"), 1)
-	entry({"node", tostring(remote.index), "config"},
+	entry({"node", remote.nodeid, "config"},
 		iframe('http://'..hostname..':'..tostring(37000+remote.index)..'/'), _("Information"), 1)
 end
