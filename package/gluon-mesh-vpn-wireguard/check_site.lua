af55905a73d7cb263db44f6985ed556e5dc8250e
lemoer <git@irrelefant.net>
2021-07-04 00:29:19 +0200

    gluon-mesh-vpn-wireguard: check_site: use precise expression for wg pubkey

--- package/gluon-mesh-vpn-wireguard/check_site.lua
+++ package/gluon-mesh-vpn-wireguard/check_site.lua
@@ -1,7 +1,7 @@
 local function check_peer(k)
 	need_alphanumeric_key(k)
 
-	need_string_match(in_domain(extend(k, {'public_key'})), "^" .. ("[%a%d+/]"):rep(43) .. "=$")
+	need_string_match(in_domain(extend(k, {'public_key'})), "^" .. ("[%a%d+/]"):rep(42) .. "[A|E|I|M|Q|U|Y|c|g|k|o|s|w|4|8|0]=$")
 	need_string(in_domain(extend(k, {'endpoint'})))
 end
 
