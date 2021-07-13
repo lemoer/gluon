8794f376962a41291e1c4178e3cb947d4e08d590
lemoer <git@irrelefant.net>
2021-07-13 22:44:30 +0200

    gluon-mesh-vpn-wireguard: shorten lua match for key (1/2)
    Co-authored-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- package/gluon-mesh-vpn-wireguard/check_site.lua
+++ package/gluon-mesh-vpn-wireguard/check_site.lua
@@ -2,7 +2,7 @@ local function check_peer(k)
 	need_alphanumeric_key(k)
 
 	need_string_match(in_domain(extend(k,
-		{'public_key'})), "^" .. ("[%a%d+/]"):rep(42) .. "[A|E|I|M|Q|U|Y|c|g|k|o|s|w|4|8|0]=$")
+		{'public_key'})), "^" .. ("[%a%d+/]"):rep(42) .. "[AEIMQUYcgkosw480]=$")
 	need_string(in_domain(extend(k, {'endpoint'})))
 end
 
